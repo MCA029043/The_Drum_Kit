1a1d8674ab50870aeb92723191a63f7d0887f8f8
MCA029043 <ajaypotlia99@gmail.com>
2023-08-20 14:51:36 +0530

    adding javascript file

---
+++ script.js
@@ -0,0 +1,32 @@
+function w(){
+   var audio = new Audio("sound/sound4.mp3");
+      audio.play();
+ }
+    function a(){
+      var audio = new Audio("sound/sound5.mp3");
+         audio.play();
+    }
+    function s(){
+      var audio = new Audio("sound/sound6.mp3");
+         audio.play();
+    }
+    function d(){
+      var audio = new Audio("sound/sound7.mp3");
+         audio.play();
+    }
+    function j(){
+      var audio = new Audio("sound/sound3.mp3");
+         audio.play();
+    }
+    function k(){
+      var audio = new Audio("sound/sound1.mp3");
+         audio.play();
+    }
+    function l(){
+      var audio = new Audio("sound/sound2.mp3");
+         audio.play();
+    }
+
+
+
+  
